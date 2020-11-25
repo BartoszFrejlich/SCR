7eee5091e17449b6f7a59a1de5e82d4393faae7b
BartoszFrejlich <bartekfrejlich@gmail.com>
2020-11-25 21:02:55 +0100

    zad1 final

--- Lab4/zad1.c
+++ Lab4/zad1.c
@@ -2,12 +2,12 @@
 #include <unistd.h> 
 #include <fcntl.h> 
 
-#define BUF_SIZE 256
+#define BUF_SIZE 64
 
-int main() {
+int main(int argc, char *argv[]) {
 
     pid_t pid;
-    int fd[2], str;
+    int fd[2];
     char buf[BUF_SIZE];
     char fname[32];
     if(pipe(fd) < 0) {
@@ -17,30 +17,28 @@ int main() {
     pid = fork();
     if(pid > 0){//parent
         close(fd[0]);
-        printf("Sciezka pliku:\n");
-        scanf("%s", fname);
 	FILE *file;
 	
-        if((file = fopen(fname, "r")) == NULL) {
-            fprintf(stderr, "nie otwarto pliku\n");
-            return 2;
+        if((file = fopen(argv[1], "r")) == NULL) {
+	  fprintf(stderr, "nie otwarto pliku\n");
+	  return 2;
 	}
        while((fgets(buf, BUF_SIZE, file)) != NULL) {    
 	 write(fd[1], buf, BUF_SIZE);
 	 }
        fclose(file);
-       wait(NULL);
        close(fd[1]);
     }
  else if(pid == 0) {//child
         close(fd[1]);
         FILE *reading_stream;
 	reading_stream = fdopen(fd[1], "r");
+	printf("\n");
 	while(read(fd[0], buf, BUF_SIZE) != 0){
-	  printf("#$s#", buf);
-	  printf("reading from pipe\n");
+	  printf("#%s#\n", buf);
 	}
 	printf("koniec odczytu\n");
 	close(fd[0]);
-    } 
+	return 0;
+    }
 }

41048613fa3fc9b6e6c42ad17cd0cb04af3cc276
BartoszFrejlich <72869990+BartoszFrejlich@users.noreply.github.com>
2020-12-22 20:07:29 +0100

    Update zad2.c

--- Lab6/zad2.c
+++ Lab6/zad2.c
@@ -1,14 +1,3 @@
-/******************************************************************************
-* FILE: hello32.c
-* DESCRIPTION:
-*   A "hello world" Pthreads program which creates a large number of
-*   threads per process.  A sleep() call is used to insure that all
-*   threads are in existence at the same time.  Each Hello thread does some
-*   work to demonstrate how the OS scheduler behavior affects thread
-*   completion order.
-* AUTHOR: Blaise Barney
-* LAST REVISED: 01/29/09
-******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,22 +5,20 @@
 #include <unistd.h>
 #include <string.h>
 #define NTHREADS 32
-
 void* Hello(void* threadid)
 {
     int i;
-    double result = 0.0;
+    double result=0.0;
     sleep(3);
-    for (i = 0; i < 10000; i++)
+    for (i=0; i<10000; i++)
     {
-        result = result + sin(i) * tan(i);
+        result=result+sin(i)*tan(i);
     }
     long tid;
     tid = (long)threadid;
     printf("%ld: Hello World!\n", tid);
     pthread_exit(NULL);
 }
-
 int main(int argc, char* argv[])
 {
     pthread_t threads[NTHREADS];
